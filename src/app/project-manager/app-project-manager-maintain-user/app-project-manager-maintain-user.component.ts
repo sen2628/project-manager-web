0ee70f290473a0812610fdeed67bf0363822cce6
137499 (Senthil) <senthilkumar.rajendran@toyota.com>
2019-01-20 22:12:51 -0600

    check in user changes

--- src/app/project-manager/app-project-manager-maintain-user/app-project-manager-maintain-user.component.ts
+++ src/app/project-manager/app-project-manager-maintain-user/app-project-manager-maintain-user.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewUsers } from '../project-manager-models/project_manager_user.model';
 
-
-
+const tempResults: ViewUsers[] = [
+  { "userId": 1, "firstName": "Senthilkumar", "lastName": "Rajendran", "employeeId": 10001 },
+  { "userId": 2, "firstName": "DineshKumar", "lastName": "Viswasam", "employeeId": 10002 },
+  { "userId": 3, "firstName": "Abirami", "lastName": "Rajendran", "employeeId": 10003 }
+]
 
 @Component({
   selector: 'app-app-project-manager-maintain-user',
@@ -16,9 +20,32 @@ export class AppProjectManagerMaintainUserComponent implements OnInit {
   newUpdateLastName: string = null;
   newUpdateEmployeeId: number = null;
 
+  resultUsersList: ViewUsers[] = [];
+
+
+  sortByColumn: string = null;
+  sortAscDscFlag: boolean;
+
+
   constructor() { }
 
   ngOnInit() {
+
+    this.resultUsersList = tempResults;
+
+  }
+
+
+  sortProjectListView(sortByString: string) {
+
+    this.sortByColumn = sortByString;
+
+    if (this.sortAscDscFlag) {
+      this.sortAscDscFlag = false;
+    } else {
+      this.sortAscDscFlag = true;
+    }
+
   }
 
 }
